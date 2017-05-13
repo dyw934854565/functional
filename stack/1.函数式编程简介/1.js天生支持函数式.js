d45ee9c857323a2a1726ae9c78c49e5abd692499
李宏吉 <m18513622209@163.com>
2017-05-14 00:23:39 +0800

    add

--- stack/1.函数式编程简介/1.js天生支持函数式.js
+++ stack/1.函数式编程简介/1.js天生支持函数式.js
@@ -1,7 +1,7 @@
 // 支持函数作为参数传入
-[1, 2, 3].forEach(console.log);
+[1, 2, 3].forEach(console.log)
 
 // 函数分离
 const splat = handle => (...array) => handle(array)
 
-console.log(splat(array => array.reduce((a, b)=> a * b))(1, 2, 3, 4))
+console.log(splat(array => array.reduce((a, b) => a * b))(1, 2, 3, 4))
--- stack/util/understore.js
+++ stack/util/understore.js
@@ -1,91 +1,68 @@
-const keys = Object.keys;
+const keys = Object.keys
 
 class _ {
-  constructor(props) {
-    this.data = props;
-    if (this instanceof _) {
-      return this
-    }
-    return new _();
-  }
 
-  /*链式操作*/
+  /* 链式操作 */
   static then(callback) {
-    this.next = callback(this.data);
-    return this;
-  };
+    this.next = callback(this.data)
+    return this
+  }
 
   static chain(props) {
-    this.data = props;
+    this.data = props
     return this
-  };
-
-  static tap(callback) {
-    callback(this.data);
-    return this
-  };
-
-  /*数组操作*/
-  static push(item) {
-    this.data.push(item);
-    return this
-  };
-
-  static find(array, condition) {
-    return array.reduce((last, next) => last = (condition(next) === true ? next: last), undefined)
   }
 
-  static reject(array, condition) {
-    return array.reduce((last, next) => {
-      if(condition(next) === false){
-        last.push(next);
-      }
-      return last;
-    }, [])
+  static tap(callback) {
+    callback(this.data)
+    return this
+  }
+
+  /* 数组操作 */
+  static push(item) {
+    console.log(this)
+    this.data.push(item)
+    return this
   }
 
   static all(...args) {
-    return args.reduceRight((truth ,f)=> (truth && f()), true);
+    return args.reduceRight((truth, f)=> (truth && f()), true)
   }
 
   static any(...args) {
-    return args.reduceRight((truth ,f)=> (truth || f()), false);
+    return args.reduceRight((truth, f)=> (truth || f()), false)
   }
 
-  /*对象操作*/
+  /* 对象操作 */
   static map(object, callback) {
     return keys(object).map((key, index) => callback(object[key], key, index, object))
   }
 
-  static sortBy(object, callback){
-    
+  /* 取值函数 */
+  static value() {
+    return this.data
   }
 
-  /*取值函数*/
-  static value(props) {
-    return this.data;
-  };
-
-  /*工具函数*/
-  static identity(key) {
-    return key
+  /* 工具函数 */
+  static identity(value) {
+    return value
   }
 
   static range(start, stop, step) {
-    var array = [];
+    const array = []
     if (start > stop) {
       for (let i = start; i >= stop; i = i - (step || 1)) {
-        array.push(i);
+        array.push(i)
       }
     } else {
       for (let i = start; i <= stop; i = i + (step || 1)) {
-        array.push(i);
+        array.push(i)
       }
     }
     return array
-  };
+  }
 }
 
-_.keys = keys;
+_.keys = keys
 
-module.exports = _;
+module.exports = _
